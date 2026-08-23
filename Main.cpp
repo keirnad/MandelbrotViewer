91a23b1e1c233ef251e3537008eb7e56aa686b6c
Dzedz Timofei <dzedz@internet.ru>
2026-08-23 17:08:56 +0300

    Shaders loading functions and compiling them

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cerrno>
+
+
+// Function for reading file contents (use it for shaders) https://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+std::string get_file_contents(const char* filename)
+{
+	std::ifstream in(filename, std::ios::in | std::ios::binary);
+	if (in)
+	{
+		std::string contents;
+		in.seekg(0, std::ios::end);
+		contents.resize(in.tellg());
+		in.seekg(0, std::ios::beg);
+		in.read(&contents[0], contents.size());
+		in.close();
+		return(contents);
+	}
+	throw(errno);
+}
 
 int main() {
 	// Init GLFW library
@@ -36,6 +59,34 @@ int main() {
 	}
 
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 800);
+
+	GLuint VBO, VAO, EBO;
+
+	// Get fragment and vertex shaders code 
+	std::string vertexCode = get_file_contents("mandelbrot.vert");
+	std::string fragmentCode = get_file_contents("mandelbrot.frag");
+
+	// Convert to C string
+	const char* vertexSource = vertexCode.c_str();
+	const char* fragmentSource = fragmentCode.c_str();
+
+	// Creating and compiling shaders
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
 
 	while (!glfwWindowShouldClose(window))
 	{
